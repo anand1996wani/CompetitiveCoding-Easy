881a6641e84abfae50cdb83b12f52a95911b6fcb
Anand Wani <anand.wani1996@gmail.com>
2019-06-22 20:25:01 +0530

    Get Node Value

---
+++ Get Node Value.java
@@ -0,0 +1,101 @@
+/*
+
+Get Node Value
+
+You’re given the pointer to the head node of a linked list and a specific position. Counting backwards from the tail node of the linked list, get the value of the node at the given position. A position of 0 corresponds to the tail, 1 corresponds to the node before the tail and so on.
+
+Input Format
+
+You have to complete the int getNode(SinglyLinkedListNode* head, int positionFromTail) method which takes two arguments - the head of the linked list and the position of the node from the tail. positionFromTail will be at least 0 and less than the number of nodes in the list. You should NOT read any input from stdin/console.
+
+The first line will contain an integer , the number of test cases. 
+Each test case has the following format: 
+The first line contains an integer , the number of elements in the linked list. 
+The next  lines contains, an element each denoting the element of the linked list. 
+The last line contains an integer  denoting the position from the tail, whose value needs to be found out and returned.
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the getNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static int getNode(SinglyLinkedListNode head, int positionFromTail) {
+            int len = 0;
+            SinglyLinkedListNode temp = head;
+            while(temp != null){
+                len = len + 1;
+                temp = temp.next;
+            }
+            int k = len - positionFromTail;
+            temp = head;
+            while(k > 1){
+                temp = temp.next;
+                k = k - 1;
+            }
+            return temp.data;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
